a9f33382b76a5ed5eb2f45580400c0e3128db575
Marius Vatasoiu <marius.vatasoiu@outlook.com>
2021-07-22 01:03:12 +0300

    chore: add prettier

---
+++ src/components/AccountSelector.tsx
@@ -0,0 +1,14 @@
+const AccontSelector = () => {
+  return (
+    <div>
+      <select>
+        <option value="usd">USD</option>
+        <option value="eur">EUR</option>
+        <option value="gbp">GBP</option>
+      </select>
+      <p>Balance: 0</p>
+    </div>
+  );
+};
+
+export default AccontSelector;
---
+++ src/components/Exchange.tsx
@@ -0,0 +1,13 @@
+import AccountSelector from "./AccountSelector";
+
+const Exchange = () => {
+  return (
+    <div>
+      <h1>Exchange</h1>
+      <AccountSelector />
+      <AccountSelector />
+    </div>
+  );
+};
+
+export default Exchange;
